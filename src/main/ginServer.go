12f5b08dfe48d51748a7bd0d68816be5d8e3e67d
Ryan Susana <ryansusana97@gmail.com>
2020-07-19 03:07:05 +0200

    added pubsub url

--- src/main/ginServer.go
+++ src/main/ginServer.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -10,6 +12,11 @@ func main() {
 
 	r.GET("/billie-jean", func(context *gin.Context) {
 		context.String(http.StatusOK, "Is not my lover!")
+	})
+
+	r.POST("/pubsub", func(context *gin.Context) {
+		all, _ := ioutil.ReadAll(context.Request.Body)
+		fmt.Println("Response: " + string(all))
 	})
 
 	r.Run()
